f1b313d759ca9d009468fc776f1e06b856140d34
Ludwig T. Tröller <ltroller@digital-village.fr>
2018-06-14 01:22:23 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,57 @@
-var SortedList = function() {};
+var SortedList = function() {
+    this.list = [];
+    this.length = 0;
+    return this;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add     = function(item) {
+    this.list.push(item);
+    this.length++;
+    this._sort();
+    return this;
+}
+SortedList.prototype.get     = function(pos) {
+   try {
+        return this.list[pos-1];
+    } catch(e) {
+        this._throwError("OutOfBounds");
+    }
+}
+SortedList.prototype.max     = function() {
+    if(this.list.length == 0) {
+        this._throwError("EmptySortedList");
+    } else {
+        return this.list[this.list.length-1];
+    }
+}
+SortedList.prototype.min     = function() {
+    if(this.list.length == 0) {
+        this._throwError("EmptySortedList");
+    } else {
+        return this.list[0];
+    }    
+}
+SortedList.prototype.average = function() {
+    if(!this.list.length) {
+        this._throwError("EmptySortedList");
+    } else {
+        return this.sum()/this.list.length;
+    }
+}
+SortedList.prototype.sum     = function() {
+    if(!this.list.length) {
+        this._throwError("EmptySortedList");
+    } else {
+        return this.list.reduce((a,b) => a + b);
+    }
+}
+
+SortedList.prototype._sort  = function() {
+    this.list.sort((a,b) => a - b);
+}
+
+SortedList.prototype._throwError = function(msg) {
+    throw new Error(msg);
+}
 
 module.exports = SortedList;
